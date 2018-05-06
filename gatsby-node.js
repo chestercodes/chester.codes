0a127b350785cbad934a15ed811daa241655f1f9
Chester Burbidge <chester.burbidge@gmail.com>
2018-05-06 11:34:47 +0100

    added redirect about -> cv

--- gatsby-node.js
+++ gatsby-node.js
@@ -76,7 +76,14 @@ exports.setFieldsOnGraphQLNodeType = ({ type, boundActionCreators }) => {
 };
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators;
+  const { createPage, createRedirect } = boundActionCreators;
+
+  createRedirect({
+    fromPath: `/about`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `/cv`,
+  });
 
   return new Promise((resolve, reject) => {
     const postPage = path.resolve("src/templates/post.jsx");
